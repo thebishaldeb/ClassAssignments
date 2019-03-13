3c1149252954557b6741c1a5fb6518a5b2523455
Bishal Deb <bishaldeb656@gmail.com>
2019-03-13 21:21:36 +0530

    Prob b final file.

--- Algorithms/Assign4/probA.py
+++ Algorithms/Assign4/probA.py
@@ -1,30 +1,32 @@
 # FUNCTIONS
 
-# We define a function to check if any sub-array is possible which gives the "sum" by
+# We define a function to check if any sub-array is possible which gives the "Z" by
 # adding, subtracting or leaving the elements from the main list
-def SubArraySum( arr, endIndex,  sum, answer):
-    if sum == 0:
+def _isSummable( arr, endIndex, Z, answer):
+    if Z == 0:
         return True
     elif endIndex >= 0 and answer == False:
-        answer = SubArraySum( arr, endIndex-1, sum, answer) # last index ignored
-        answer = SubArraySum( arr, endIndex-1, sum - arr[endIndex], answer) # subtracting last index value
-        answer = SubArraySum( arr, endIndex-1, sum + arr[endIndex], answer) # adding last index value from main array
+        answer = _isSummable( arr, endIndex-1, Z, answer) # last index ignored
+        answer = _isSummable( arr, endIndex-1, Z - arr[endIndex], answer) # subtracting last index value
+        answer = _isSummable( arr, endIndex-1, Z + arr[endIndex], answer) # adding last index value from main array
     return answer
 
+# IsSummable(Z, A) from the problem statement
+def isSummable( Z, A):
+    N = len(A) # Total no of elements in the list
+    answer = False # Answer initialized to false
+    answer = _isSummable( A , N-1, Z, answer)
+    if answer == True:
+        print("\nYes, it is possible to find a subset from the main set which gives the required value of Z.")
+    else:
+        print("\nNo, it is not possible to find a subset from the main set which gives the required value of Z.")
+
 # MAIN PROGRAM
-ListOFElements = ( 3, 6, 2, 4, -1, -7) # The main list from which sum needs to be computed 
+A = ( 3, 6, 2, 4, -1, -7) # The main list from which sum needs to be computed 
 
-answer = False # Answer initialized to false
+print("Given List is", A)
 
-print("Given List is", ListOFElements)
+Z = int(input("\nEnter the value of Z that needs to be checked "))
 
-sum = int(input("\nEnter the sum that needs to be checked "))
+isSummable( Z, A) # The function to check the required condition
 
-N = len(ListOFElements) # Total no of elements in the list
-
-answer = SubArraySum( ListOFElements , N-1, sum, answer)
-
-if answer == True:
-    print("\nYes, elements from the main list give the required sum.")
-else:
-    print("\nNo, no elements of the main list give the required sum.")
